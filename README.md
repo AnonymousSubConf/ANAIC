8979222662377ffa6f326f8f0c6e8dd2f8dd15a9
AnonymousSubConf <141832649+AnonymousSubConf@users.noreply.github.com>
2023-09-28 23:19:07 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ANAIC
-Partial code of ANA-IC
+#  Code For ANA-IC
+
+## Require
+
+- Python 3.9
+- PyTorch 1.10.2
+- dgl 0.7.2
+- numpy  1.20.3
+
+The  hyper-parameters for ANA-IC are as follows:
+```python
+python main.py --name_dataset cora --num_epochs 50 --pf 0.1 --pe 0.45 --lr2 1e-2 --wd2 1e-4 --K 1 --t 3 --lambd 100
+
+python main.py --name_dataset pubmed --num_epochs 100 --pf 0.0 --pe 0.6 --lr2 1e-2 --wd2 1e-4  --K 3 --t -4 --lambd 280
+
+python main.py --name_dataset citeseer --num_epochs 10 --num_layer 1 --pf 0.0 --pe 0.35 --lr2 1e-2 --wd2 1e-2  --K 2 --t 5 --lambd 30
+```
+
---
+++ main.py
@@ -0,0 +1,186 @@
+import argparse
+import sys
+from model import Pipeline, LogReg
+from dataset import load_dataset
+import numpy as np
+import torch
+import torch.nn as nn
+from utils import *
+import warnings
+import time
+import os.path as osp
+import os
+import torch
+
+warnings.filterwarnings('ignore')
+
+parser = argparse.ArgumentParser(description='ANA-IC')
+
+parser.add_argument('--name_dataset', type=str, default='cs', help='Dataset for train and test.')
+parser.add_argument('--gpu_index', type=int, default=0, help='Choosen GPU index.')
+parser.add_argument('--num_epochs', type=int, default=40, help='Epochs for training model.')
+parser.add_argument('--lr1', type=float, default=1e-3, help='Learning rate of GCN backbone.')
+parser.add_argument('--lr2', type=float, default=5e-3, help='Learning rate of logistic regression model.')
+parser.add_argument('--wd1', type=float, default=0, help='Weight decay of GCN backbone.')
+parser.add_argument('--wd2', type=float, default=1e-4, help='Weight decay of logistic regression model.')
+parser.add_argument('--lambd', type=float, default=1e-3, help='Coefficient for dimension decorrelation.')
+parser.add_argument('--num_layers', type=int, default=2, help='Number of GNN of MLP layers')
+parser.add_argument('--pe', type=float, default=1.0, help='Drop ratio for edge.')
+parser.add_argument('--pf', type=float, default=0.1, help='Drop ratio for node feature.')
+parser.add_argument("--hid_dim", type=int, default=512, help='Hidden dimension.')
+parser.add_argument("--out_dim", type=int, default=512, help='Output dimemsion.')
+parser.add_argument("--K", type=int, default=1, help='Neighbor hops.')
+parser.add_argument("--t", type=float, default=1.0, help='Damping coefficient.')
+
+args = parser.parse_args()
+
+
+
+def generate_orthogonal_matrix(d):
+    x = torch.randn(d, d)
+    q, _ = torch.qr(x)
+    return q
+
+def generate_orthogonal_matrix_(d):
+    u = torch.rand(d, 1)
+    u = u / torch.norm(u)
+    q = torch.eye(d) - 2 * u @ u.T
+    return q
+
+
+
+if __name__ == '__main__':
+    # choose gpu or cpu
+    if args.gpu_index >= 0 and torch.cuda.is_available():
+        args.device = 'cuda:{}'.format(args.gpu_index)
+    else:
+        args.device = 'cpu'
+    print("super-parameters are as follows:", args)
+    graph, feature, labels, num_classes, train_idx, val_idx, test_idx = load_dataset(args.name_dataset)
+    in_dim = feature.shape[1]
+
+    use_mlp = False
+    if args.name_dataset == "cs":
+        use_mlp = True
+
+    model = Pipeline(in_dim, args.hid_dim, args.out_dim, args.num_layers, use_mlp)
+    model = model.to(args.device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr1, weight_decay=args.wd1)
+
+    N = graph.number_of_nodes()
+    
+    I = torch.eye(N, dtype=torch.float)
+    adj = (graph.adjacency_matrix().to_dense() + I)
+    d = torch.sum(adj, dim=1)
+    d_inv = 1 / d
+    d_inv[d_inv == np.inf] = 0.0
+    D_inv = torch.diag(d_inv)
+    D = torch.diag(d)
+
+
+    W = gene_affinity(A=adj, K=args.K, t=args.t).to(args.device)  
+    
+    I_D = torch.eye(args.out_dim, dtype=torch.float).to(args.device)
+                                   
+        
+    print("Start training!")
+    starttime = time.time()
+    for epoch in range(args.num_epochs):
+        model.train()
+        optimizer.zero_grad()
+
+        graph1, feature1 = augment_a_graph(graph, feature, args.pf, args.pe)
+        graph2, feature2 = augment_a_graph(graph, feature, args.pf, args.pe)
+
+        graph1 = graph1.add_self_loop()
+        graph2 = graph2.add_self_loop()
+        graph1 = graph1.to(args.device)
+        graph2 = graph2.to(args.device)
+
+        feature1 = feature1.to(args.device)
+        feature2 = feature2.to(args.device)
+
+        h1, h2 = model(graph1, feature1, graph2, feature2)
+
+        c = (torch.mm(torch.mm(h1.T, W), h2)+torch.mm(torch.mm(h2.T, W), h1)) * 0.5  /    N
+        loss_ana = - torch.diagonal(c).sum()
+        
+        q1 = generate_orthogonal_matrix(args.out_dim).to(args.device)
+        q2 = generate_orthogonal_matrix(args.out_dim).to(args.device)
+        h1 = h1 @ q1 
+        h2 = h2 @ q2 
+        var1 = torch.var(h1, dim=0)
+        var2 = torch.var(h2, dim=0)
+        loss_ic1 = torch.var(var1)
+        loss_ic2 = torch.var(var2)
+        
+        loss = loss_ana + args.lambd * (loss_ic1 + loss_ic2)
+        
+        loss.backward()
+        optimizer.step()
+
+        # print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss.item()))
+    
+    finishtime = time.time()
+    print("Finish training!")
+    
+    print("=== Evaluation ===")
+    graph_ = graph.to(args.device)
+    graph_ = graph_.remove_self_loop().add_self_loop()
+    feature_ = feature.to(args.device)
+
+    embeds = model.get_embedding(graph_, feature_)    
+
+    train_embs = embeds[train_idx]
+    val_embs = embeds[val_idx]
+    test_embs = embeds[test_idx]
+
+    label = labels.to(args.device)
+    
+    train_labels = label[train_idx]
+    val_labels = label[val_idx]
+    test_labels = label[test_idx]
+    
+    
+    ''' Linear Evaluation '''
+    logreg = LogReg(train_embs.shape[1], num_classes)
+    opt = torch.optim.Adam(logreg.parameters(), lr=args.lr2, weight_decay=args.wd2)
+
+    logreg = logreg.to(args.device)
+    loss_fn = nn.CrossEntropyLoss()
+
+    best_val_acc = 0
+    eval_acc = 0
+
+    for epoch in range(2000):
+        logreg.train()
+        opt.zero_grad()
+        logits = logreg(train_embs)
+        preds = torch.argmax(logits, dim=1)
+        train_acc = torch.sum(preds == train_labels).float() / train_labels.shape[0]
+        loss = loss_fn(logits, train_labels)
+        loss.backward()
+        opt.step()
+
+        logreg.eval()
+        with torch.no_grad():
+            val_logits = logreg(val_embs)
+            test_logits = logreg(test_embs)
+
+            val_preds = torch.argmax(val_logits, dim=1)
+            test_preds = torch.argmax(test_logits, dim=1)
+
+            val_acc = torch.sum(val_preds == val_labels).float() / val_labels.shape[0]
+            test_acc = torch.sum(test_preds == test_labels).float() / test_labels.shape[0]
+
+            if val_acc >= best_val_acc:
+                best_val_acc = val_acc              
+                eval_acc = test_acc
+
+            # print('Epoch:{}, train_acc:{:.4f}, val_acc:{:4f}, test_acc:{:4f}'.format(epoch, train_acc, val_acc, test_acc))
+
+    print('Linear evaluation accuracy:{:.4f}'.format(eval_acc))
+
+
+ 
